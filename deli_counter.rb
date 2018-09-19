e7fc95b9f14ddb7e942acfa4b737ec4a43814dbf
Duane Felton <dfelton2010@gmail.com>
2018-09-19 15:12:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,6 @@
 
 def line(katz_deli)
   if katz_deli.length != 0
-    katz_deli.each_with_index { |v, i| puts "The line is currently: #{i}. "}
+    katz_deli.each_with_index { |v, i| puts "The line is currently: #{i+1}. #{v} "}
   end
 end
