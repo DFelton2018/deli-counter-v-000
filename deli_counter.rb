7c406babe1fad3f20bd0fdd1e4a0489d1c6a12b8
Duane Felton <dfelton2010@gmail.com>
2018-09-19 15:36:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,8 @@
 # x.each_with_index { |v, i| puts "current index...#{i}" }
 
 def line(katz_deli)
-  count = 1
-  while katz_deli.length > 0
-  katz_deli.each do |v| 
+    count = 1
+    katz_deli.each do |v| 
     puts "The line is currently: #{count}. #{v[0..-1]} "
     count += 1
   end
