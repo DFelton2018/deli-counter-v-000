de12f03bdfd8c8b8a79a07a05f24743852886336
Duane Felton <dfelton2010@gmail.com>
2018-09-19 15:28:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@
 # x.each_with_index { |v, i| puts "current index...#{i}" }
 
 def line(katz_deli)
-    katz_deli.each_with_index do |v, i| 
-      puts "The line is currently: #{i+1}. #{v} "
+  katz_deli.each_with_index do |v, i| 
+    puts "The line is currently: #{i+1}. #{v} "
   end
 
