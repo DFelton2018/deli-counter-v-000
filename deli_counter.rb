835cbf42c476adef1f35b385195f1da6461c51f8
Duane Felton <dfelton2010@gmail.com>
2018-09-19 14:54:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,15 +11,6 @@ def line(katz_deli)
     katz_deli.each_with_index do |item, index|
     puts "The line is currently: #{index+1}. #{item} "
    end 
- 
+ end
 
 
-def take_a_number(katz_deli,customer)
-  katz_deli.push(customer)
-end
-
-def now_serving(katz_deli)
-  puts "Currently serving #{katz_deli.first}."
-end
-end
-end
