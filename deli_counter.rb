099b9ebe985c53b481cf46b8b0a64ff6baec0294
Duane Felton <dfelton2010@gmail.com>
2018-09-19 14:05:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(katz_deli)
+end  
+
+
+def take_a_number(katz_deli,customer)
+  katz_deli.push(customer)
+end
+
+def now_serving(katz_deli)
+  puts "Currently serving #{katz_deli[0]}."
+end
+
+end
