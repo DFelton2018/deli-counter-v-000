5710da57b1647324a6b305b331a40782960783b2
Duane Felton <dfelton2010@gmail.com>
2018-09-19 14:50:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,16 +2,14 @@
 #   puts "current_index: #{index}"
 # end
 
-
-
-
-
 def line(katz_deli)
+  
   if katz_deli.length == 0 
     puts "The line is currently empty."
+    
   elsif katz_deli.length > 0
-    katz_deli.each_with_index do |customer, index|
-    puts "The line is currently:  #{katz_deli.join(" ") } "
+    katz_deli.each_with_index do |item, index|
+    puts "The line is currently: #{index}. #{item} "
     
 end  
 end
