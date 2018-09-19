7bd849914b6125632523e64d48fde59deb785d47
Duane Felton <dfelton2010@gmail.com>
2018-09-19 14:53:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(katz_deli)
   while katz_deli.length > 0
     katz_deli.each_with_index do |item, index|
     puts "The line is currently: #{index+1}. #{item} "
-    
+   end 
 end  
 end
 
